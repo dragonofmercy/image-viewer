364d7626048fcf57bc210f9339c628af3271abb2
Dragon <dragonofmercy@gmail.com>
2022-09-08 15:05:00 +0200

    Add settings

---
+++ ImageViewer/Library/Settings.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.Win32;
+
+namespace ImageViewer
+{
+    internal class Settings
+    {
+        internal const string HkeyAppPath = @"HKEY_CURRENT_USER\SOFTWARE\Dragon Industries\Image Viewer";
+        internal const string HValueAppTheme = "AppTheme";
+        
+        public static ElementTheme Theme 
+        {
+            get
+            {
+                int theme = (int)Registry.GetValue(HkeyAppPath, HValueAppTheme, ElementTheme.Default);
+                return (ElementTheme)theme;
+            }
+
+            set
+            {
+                Registry.SetValue(HkeyAppPath, HValueAppTheme, (int)value);
+            }
+         }
+    }
+}
