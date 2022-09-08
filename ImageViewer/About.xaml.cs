c4cc0ab71fe4fb76b035e432786f0670bd94f614
Dragon <dragonofmercy@gmail.com>
2022-09-08 15:04:50 +0200

    Add about

---
+++ ImageViewer/About.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace ImageViewer
+{
+    public sealed partial class About : Page
+    {
+        private readonly ContentDialog Dialog;
+
+        public About(ContentDialog e)
+        {
+            InitializeComponent();
+
+            e.Opened += DialogOpened;
+            Dialog = e;
+        }
+
+        private void DialogOpened(ContentDialog sender, ContentDialogOpenedEventArgs args)
+        {
+            CheckRemoteVersion();
+        }
+
+        private async void CheckRemoteVersion()
+        {
+            string remote_version = await Update.GetRemoteVersion();
+            TextRemoteVersion.Text = remote_version;
+
+            if(string.Compare(remote_version, Update.GetCurrentVersion(), StringComparison.InvariantCulture) > 0)
+            {
+                ButtonUpdate.IsEnabled = true;
+            }
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Dialog.Hide();
+        }
+    }
+}
