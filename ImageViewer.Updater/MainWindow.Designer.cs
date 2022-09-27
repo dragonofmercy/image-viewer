753c125620dd6c962b92d970e167166ec392477c
Dragon <dragonofmercy@gmail.com>
2022-09-27 14:32:31 +0200

    Add new installer project

---
+++ ImageViewer.Updater/MainWindow.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ImageViewer.Updater
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TextInstallStatus = new System.Windows.Forms.Label();
+            this.ProgressStatus = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ImageViewer.Updater.Properties.Resources.AppIcon64;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(109, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Image Viewer x64";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label2.Location = new System.Drawing.Point(18, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 37);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Setup Progress";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Processing:";
+            // 
+            // TextInstallStatus
+            // 
+            this.TextInstallStatus.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextInstallStatus.Location = new System.Drawing.Point(113, 148);
+            this.TextInstallStatus.Name = "TextInstallStatus";
+            this.TextInstallStatus.Size = new System.Drawing.Size(505, 17);
+            this.TextInstallStatus.TabIndex = 4;
+            this.TextInstallStatus.Text = "Initialization";
+            // 
+            // ProgressStatus
+            // 
+            this.ProgressStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ProgressStatus.Location = new System.Drawing.Point(25, 180);
+            this.ProgressStatus.Name = "ProgressStatus";
+            this.ProgressStatus.Size = new System.Drawing.Size(593, 23);
+            this.ProgressStatus.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.ProgressStatus.TabIndex = 5;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(647, 226);
+            this.Controls.Add(this.ProgressStatus);
+            this.Controls.Add(this.TextInstallStatus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.Text = "Image Viewer Installer";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label TextInstallStatus;
+        private System.Windows.Forms.ProgressBar ProgressStatus;
+    }
+}
+
---
+++ ImageViewer.Updater/MainWindow.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ImageViewer.Updater
+{
+    public partial class MainWindow : Form
+    {
+        const uint MAX_DOWNLOAD_ATTEMPTS = 3;
+        const string GITHUB_API_RELEASE_PATH = "https://api.github.com/repos/dragonofmercy/image-viewer/releases/latest";
+        const string RUNTIME_DOWNLOAD = "https://aka.ms/windowsappsdk/1.1/1.1.5/windowsappruntimeinstall-x64.exe";
+        const string NET6_RUNTIME_DOWNLOAD = "https://download.visualstudio.microsoft.com/download/pr/fe8415d4-8a35-4af9-80a5-51306a96282d/05f9b2a1b4884238e69468e49b3a5453/windowsdesktop-runtime-6.0.9-win-x64.exe";
+
+        private string TempDirectory;
+        private string InstallSourcesDirectory;
+        private string InstallDestinationDirectory;
+        private string ShortcutFilePath;
+        private string Callable;
+
+        private readonly Timer TimerWait = new Timer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            TempDirectory = Path.Combine(Path.GetTempPath(), "imageviewer_install");
+            InstallSourcesDirectory = Path.Combine(TempDirectory, "update");
+            InstallDestinationDirectory = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly().Location).CompanyName
+            );
+
+            ShortcutFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), "Programs", "Image Viewer.lnk");
+
+            if(Directory.Exists(TempDirectory))
+            {
+                Directory.Delete(TempDirectory, true);
+            }
+
+            Directory.CreateDirectory(TempDirectory);
+
+            TimerWait.Tick += TimerWait_Tick;
+
+            Callable = "CheckAppRuntime";
+            TimerWait.Interval = 2000;
+            TimerWait.Start();
+        }
+
+        private void TimerWait_Tick(object sender, EventArgs e)
+        {
+            (sender as Timer).Stop();
+
+            Type t = GetType();
+            MethodInfo mi = t.GetMethod(Callable);
+            mi.Invoke(this, null);
+        }
+
+        private async Task<bool> DownloadFile(string Url, string SavePath)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "Image Viewer Updater");
+
+            for(uint i = 0; i < MAX_DOWNLOAD_ATTEMPTS; i++)
+            {
+                try
+                {
+                    Application.DoEvents();
+
+                    Stream s = await httpClient.GetStreamAsync(new Uri(Url));
+                    FileStream fs = new FileStream(SavePath, FileMode.CreateNew);
+
+                    await s.CopyToAsync(fs);
+
+                    fs.Dispose();
+                    s.Dispose();
+                    httpClient.Dispose();
+
+                    return true;
+                }
+                catch(Exception)
+                {
+                }
+            }
+
+            httpClient.Dispose();
+            return false;
+        }
+
+        public void CheckAppRuntime()
+        {
+            TextInstallStatus.Text = "Checking Windows AppRuntime...";
+
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "powershell",
+                Arguments = "get-appxpackage *appruntime* | Select-String 'WindowsAppRuntime.1.1_1005'",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+
+            Process process = new Process
+            {
+                StartInfo = psi
+            };
+
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.Dispose();
+
+            if(string.IsNullOrEmpty(output))
+            {
+                Callable = "InstallRuntime";
+                TimerWait.Interval = 1000;
+                TimerWait.Start();
+            }
+            else
+            {
+                Callable = "CheckNet6";
+                TimerWait.Interval = 1000;
+                TimerWait.Start();
+            }
+        }
+
+        public async void InstallRuntime()
+        {
+            TextInstallStatus.Text = "Download Windows AppRuntime...";
+
+            if(await DownloadFile(RUNTIME_DOWNLOAD, Path.Combine(TempDirectory, "windowsappruntime.exe")))
+            {
+                TextInstallStatus.Text = "Installing Windows AppRuntime...";
+
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    UseShellExecute = false,
+                    FileName = Path.Combine(TempDirectory, "windowsappruntime.exe")
+                };
+
+                Process process = new Process
+                {
+                    StartInfo = psi
+                };
+
+                process.Start();
+
+                while(!process.HasExited)
+                {
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(100);
+                }
+
+                process.Dispose();
+
+                Callable = "CheckNet6";
+                TimerWait.Interval = 1000;
+                TimerWait.Start();
+            }
+            else
+            {
+                throw new Exception("Cannot download Windows AppRuntime, please try again later...");
+            }
+        }
+
+        public void CheckNet6()
+        {
+            TextInstallStatus.Text = "Checking .NET 6.0 Desktop Runtime...";
+
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "powershell",
+                Arguments = "dotnet --list-runtimes | Select-String 'WindowsDesktop.App 6.0.9'",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+
+            Process process = new Process
+            {
+                StartInfo = psi
+            };
+
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.Dispose();
+
+            if(string.IsNullOrEmpty(output) || output.StartsWith("dotnet : "))
+            {
+                Callable = "InstallNet6";
+                TimerWait.Interval = 1000;
+                TimerWait.Start();
+            }
+            else
+            {
+                Callable = "Download";
+                TimerWait.Interval = 1000;
+                TimerWait.Start();
+            }
+        }
+
+        public async void InstallNet6()
+        {
+            TextInstallStatus.Text = "Downloading .NET 6.0 Desktop Runtime...";
+
+            if(await DownloadFile(NET6_RUNTIME_DOWNLOAD, Path.Combine(TempDirectory, "windowsdesktopruntime.exe")))
+            {
+                TextInstallStatus.Text = "Installing .NET 6.0 Desktop Runtime...";
+
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    UseShellExecute = false,
+                    Arguments = "/install /quiet /norestart",
+                    FileName = Path.Combine(TempDirectory, "windowsdesktopruntime.exe")
+                };
+
+                Process process = new Process
+                {
+                    StartInfo = psi
+                };
+
+                process.Start();
+
+                while(!process.HasExited)
+                {
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(100);
+                }
+
+                process.Dispose();
+
+                Callable = "Download";
+                TimerWait.Interval = 1000;
+                TimerWait.Start();
+            }
+            else
+            {
+                throw new Exception("Cannot download .NET 6.0 Desktop Runtime, please try again later...");
+            }
+        }
+
+        public async void Download()
+        {
+            TextInstallStatus.Text = "Downloading Image Viewer...";
+
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "Image Viewer Updater");
+
+            string responseContent;
+
+            try
+            {
+                HttpResponseMessage responseMessage = await httpClient.GetAsync(GITHUB_API_RELEASE_PATH);
+                responseContent = await responseMessage.Content.ReadAsStringAsync();
+                httpClient.Dispose();
+            }
+            catch(Exception)
+            {
+                httpClient.Dispose();
+                throw new Exception("No internet access");
+            }
+
+            Regex reg = new Regex("\"browser_download_url\":\"((\\\\\"|[^\"])*release.zip)\"", RegexOptions.IgnoreCase);
+            MatchCollection matches = reg.Matches(responseContent);
+
+            if(matches.Count == 1)
+            {
+                if(await DownloadFile(matches[0].Groups[1].Value, Path.Combine(TempDirectory, "release.zip")))
+                {
+                    TextInstallStatus.Text = "Extracting files...";
+
+                    Callable = "Extract";
+                    TimerWait.Interval = 1000;
+                    TimerWait.Start();
+                }
+                else
+                {
+                    throw new Exception("Cannot download remote version");
+                }
+            }
+            else
+            {
+                throw new Exception("Cannot get remote package");
+            }
+        }
+
+        public void Extract()
+        {
+            if(Directory.Exists(InstallSourcesDirectory))
+            {
+                Directory.Delete(InstallSourcesDirectory, true);
+            }
+
+            ZipFile.ExtractToDirectory(Path.Combine(TempDirectory, "release.zip"), InstallSourcesDirectory);
+
+            TextInstallStatus.Text = "Installing files...";
+
+            Callable = "Install";
+            TimerWait.Interval = 1000;
+            TimerWait.Start();
+        }
+
+        public void Install()
+        {
+            if(Directory.Exists(InstallDestinationDirectory))
+            {
+                Directory.Delete(InstallDestinationDirectory, true);
+            }
+
+            Directory.CreateDirectory(InstallDestinationDirectory);
+            Directory.Move(InstallSourcesDirectory, Path.Combine(InstallDestinationDirectory, "Image Viewer"));
+
+            if(File.Exists(ShortcutFilePath))
+            {
+                File.Delete(ShortcutFilePath);
+            }
+
+            IShellLink link = (IShellLink)new ShellLink();
+            link.SetPath(Path.Combine(InstallDestinationDirectory, "Image Viewer", "ImageViewer.exe"));
+
+            IPersistFile file = (IPersistFile)link;
+            file.Save(ShortcutFilePath, false);
+
+            TextInstallStatus.Text = "Cleaning up...";
+
+            Callable = "Cleanup";
+            TimerWait.Interval = 1000;
+            TimerWait.Start();
+        }
+
+        public void Cleanup()
+        {
+            Directory.Delete(TempDirectory, true);
+
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                UseShellExecute = true,
+                FileName = Path.Combine(InstallDestinationDirectory, "Image Viewer", "ImageViewer.exe")
+            };
+
+            Process process = new Process
+            {
+                StartInfo = psi
+            };
+
+            process.Start();
+            Environment.Exit(0);
+        }
+    }
+}
