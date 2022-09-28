822ebd26140517b00697acc6f0e09ba9d05b7f91
Dragon <dragonofmercy@gmail.com>
2022-09-28 14:39:00 +0200

    Update assembly

--- ImageViewer.Updater/Properties/AssemblyInfo.cs
+++ ImageViewer.Updater/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.22.9.27")]
+[assembly: AssemblyFileVersion("1.22.9.28")]
