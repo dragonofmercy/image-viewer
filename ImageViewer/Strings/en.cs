c5a052e9932344d17850db3324c2d9e8c9f71d16
Dragon <dragonofmercy@gmail.com>
2023-04-27 09:59:00 +0200

    Move localization

---
+++ ImageViewer/Strings/en.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+
+namespace ImageViewer.Strings;
+
+internal static class En
+{
+    public static Dictionary<string, string> GetStrings()
+    {
+        return new Dictionary<string, string>(){
+
+            { "DEFAULT_SYSTEM_LANGUAGE", "System language" },
+            { "SYSTEM_PASTED_CONTENT", "Pasted content" },
+            { "SYSTEM_LOADING_ERROR", "Sorry, the application cannot open this file because the format is currently not supported or the file is damaged" },
+
+            { "SETTINGS_FIELD_LANGUAGE", "Language" },
+            { "SETTINGS_FIELD_LANGUAGE_HELP", "You need to restart the application to apply this setting." },
+            { "SETTINGS_FIELD_UPDATE_INTERVAL", "Auto update interval" },
+            { "SETTINGS_FIELD_UPDATE_INTERVAL_DAY", "Every days" },
+            { "SETTINGS_FIELD_UPDATE_INTERVAL_WEEK", "Every weeks" },
+            { "SETTINGS_FIELD_UPDATE_INTERVAL_MONTH", "Every months" },
+            { "SETTINGS_FIELD_UPDATE_INTERVAL_MANUAL", "Manually" },
+
+            { "FILE_INFORMATION_TITLE", "File informations" },
+            { "FILE_INFORMATION_DIMENSIONS", "File dimensions" },
+            { "FILE_INFORMATION_FOLDER_PATH", "Folder path" },
+            { "FILE_INFORMATION_FOLDER_PATH_TIP", "Display directory in Windows Explorer" },
+
+            { "FOOTER_TOOLBAR_MENU", "Menu" },
+            { "FOOTER_TOOLBAR_MENU_FILE_OPEN", "Open image" },
+            { "FOOTER_TOOLBAR_MENU_FILE_INFO", "File info" },
+            { "FOOTER_TOOLBAR_MENU_FILE_SAVE", "Save as..." },
+            { "FOOTER_TOOLBAR_MENU_FILE_SAVE_FORMAT", "{0} image" },
+            { "FOOTER_TOOLBAR_MENU_FILE_DELETE", "Delete image" },
+            { "FOOTER_TOOLBAR_MENU_ABOUT", "About" },
+            { "FOOTER_TOOLBAR_MENU_OPTIONS", "Settings" },
+            { "FOOTER_TOOLBAR_MENU_QUIT", "Quit" },
+
+            { "FOOTER_TOOLBAR_IMAGE_ADJUST", "Zoom to fit" },
+            { "FOOTER_TOOLBAR_IMAGE_ZOOM_100", "Zoom to actual size" },
+            { "FOOTER_TOOLBAR_IMAGE_PREVIOUS", "Previous image" },
+            { "FOOTER_TOOLBAR_IMAGE_ZOOM_IN", "Zoom in" },
+            { "FOOTER_TOOLBAR_IMAGE_ZOOM_OUT", "Zoom out" },
+            { "FOOTER_TOOLBAR_IMAGE_NEXT", "Next image" },
+
+            { "FOOTER_TOOLBAR_TRANSFORM_MENU", "Transform image" },
+            { "FOOTER_TOOLBAR_TRANSFORM_ROTATE_LEFT", "Rotate left" },
+            { "FOOTER_TOOLBAR_TRANSFORM_ROTATE_RIGHT", "Rotate right" },
+            { "FOOTER_TOOLBAR_TRANSFORM_FLIP_HORZ", "Flip horizontal" },
+            { "FOOTER_TOOLBAR_TRANSFORM_FLIP_VERT", "Flip vertical" },
+
+            { "ABOUT_LINK_GITHUB_REPOSITORY", "GitHub Repository" },
+            { "ABOUT_LINK_LATEST_RELEASE", "Latest releases" },
+            { "ABOUT_LABEL_LAST_UPDATE", "Last checked: " },
+            { "ABOUT_LABEL_LAST_UPDATE_NEVER", "never" },
+
+            { "ABOUT_BTN_CHECK_UPDATE", "Check for update" },
+            { "ABOUT_BTN_DOWNLOAD_UPDATE", "Download update" },
+            { "ABOUT_BTN_DOWNLOAD_UPDATE_DOWNLOADING", "Downloading..." },
+            { "ABOUT_BTN_DOWNLOAD_UPDATE_RETRY", "Retry" },
+
+            { "ABOUT_UPDATE_CHECKING", "Checking for update..." },
+            { "ABOUT_UPDATE_INFO_UPDATE_LATEST", "Image Viewer is up to date." },
+            { "ABOUT_UPDATE_INFO_UPDATE_AVAILABLE", "An update is available." },
+            { "ABOUT_UPDATE_INFO_ERROR_NO_INTERNET", "No internet access." },
+            { "ABOUT_UPDATE_INFO_ERROR_KEY_NOT_FOUND", "Cannot get last version manifest." },
+
+        };
+    }
+}
---
+++ ImageViewer/Strings/fr.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+
+namespace ImageViewer.Strings;
+
+internal static class Fr
+{
+    public static Dictionary<string, string> GetStrings()
+    {
+        return new Dictionary<string, string>(){
+
+            { "DEFAULT_SYSTEM_LANGUAGE", "Langue du système" },
+            { "SYSTEM_PASTED_CONTENT", "Contenu collé" },
+            { "SYSTEM_LOADING_ERROR", "Désolé, l'application ne peut pas ouvrir ce fichier car le format n'est actuellement pas pris en charge ou le fichier est endommagé" },
+
+            { "SETTINGS_FIELD_LANGUAGE", "Langue" },
+            { "SETTINGS_FIELD_LANGUAGE_HELP", "Vous devez redémarrer l'application pour appliquer ce paramètre." },
+            { "SETTINGS_FIELD_UPDATE_INTERVAL", "Recherche des mises à jour" },
+            { "SETTINGS_FIELD_UPDATE_INTERVAL_DAY", "Tous les jours" },
+            { "SETTINGS_FIELD_UPDATE_INTERVAL_WEEK", "Toutes les semaines" },
+            { "SETTINGS_FIELD_UPDATE_INTERVAL_MONTH", "Tous les mois" },
+            { "SETTINGS_FIELD_UPDATE_INTERVAL_MANUAL", "Manuellement" },
+
+            { "FILE_INFORMATION_TITLE", "Informations de l'image" },
+            { "FILE_INFORMATION_DIMENSIONS", "Dimension de l'image" },
+            { "FILE_INFORMATION_FOLDER_PATH", "Chemin du dossier" },
+            { "FILE_INFORMATION_FOLDER_PATH_TIP", "Afficher le répertoire dans l'explorateur Windows" },
+
+            { "FOOTER_TOOLBAR_MENU", "Menu" },
+            { "FOOTER_TOOLBAR_MENU_FILE_OPEN", "Ouvrir une image" },
+            { "FOOTER_TOOLBAR_MENU_FILE_INFO", "Informations de l'image" },
+            { "FOOTER_TOOLBAR_MENU_FILE_SAVE", "Enregistrer sous..." },
+            { "FOOTER_TOOLBAR_MENU_FILE_SAVE_FORMAT", "Image {0}" },
+            { "FOOTER_TOOLBAR_MENU_FILE_DELETE", "Supprimer l'image" },
+            { "FOOTER_TOOLBAR_MENU_ABOUT", "A propos" },
+            { "FOOTER_TOOLBAR_MENU_OPTIONS", "Options" },
+            { "FOOTER_TOOLBAR_MENU_QUIT", "Quitter" },
+
+            { "FOOTER_TOOLBAR_IMAGE_ADJUST", "Faire un zoom pour ajuster" },
+            { "FOOTER_TOOLBAR_IMAGE_ZOOM_100", "Zoom en taille réelle" },
+            { "FOOTER_TOOLBAR_IMAGE_PREVIOUS", "Image précédente" },
+            { "FOOTER_TOOLBAR_IMAGE_ZOOM_IN", "Zoom avant" },
+            { "FOOTER_TOOLBAR_IMAGE_ZOOM_OUT", "Zoom arrière" },
+            { "FOOTER_TOOLBAR_IMAGE_NEXT", "Image suivante" },
+
+            { "FOOTER_TOOLBAR_TRANSFORM_MENU", "Transformation de l'image" },
+            { "FOOTER_TOOLBAR_TRANSFORM_ROTATE_LEFT", "Rotation gauche" },
+            { "FOOTER_TOOLBAR_TRANSFORM_ROTATE_RIGHT", "Rotation droite" },
+            { "FOOTER_TOOLBAR_TRANSFORM_FLIP_HORZ", "Mirroir horizontal" },
+            { "FOOTER_TOOLBAR_TRANSFORM_FLIP_VERT", "Mirroir vertical" },
+
+            { "ABOUT_LINK_GITHUB_REPOSITORY", "Dépôt GitHub" },
+            { "ABOUT_LINK_LATEST_RELEASE", "Dernières versions" },
+            { "ABOUT_LABEL_LAST_UPDATE", "Dernière vérification : " },
+            { "ABOUT_LABEL_LAST_UPDATE_NEVER", "jamais" },
+
+            { "ABOUT_BTN_CHECK_UPDATE", "Vérifier les mises à jour" },
+            { "ABOUT_BTN_DOWNLOAD_UPDATE", "Télecharger la mise à jour" },
+            { "ABOUT_BTN_DOWNLOAD_UPDATE_DOWNLOADING", "Télechargement en cours..." },
+            { "ABOUT_BTN_DOWNLOAD_UPDATE_RETRY", "Réessayer" },
+
+            { "ABOUT_UPDATE_CHECKING", "Recherche en cours..." },
+            { "ABOUT_UPDATE_INFO_UPDATE_LATEST", "Image Viewer est à jour." },
+            { "ABOUT_UPDATE_INFO_UPDATE_AVAILABLE", "Une mise à jour est disponible." },
+            { "ABOUT_UPDATE_INFO_ERROR_NO_INTERNET", "Pas d'accès à internet." },
+            { "ABOUT_UPDATE_INFO_ERROR_KEY_NOT_FOUND", "Impossible de récupèrer la dernière version." },
+
+        };
+    }
+}
