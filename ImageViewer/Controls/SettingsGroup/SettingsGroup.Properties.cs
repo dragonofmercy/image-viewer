cb6cc707316b7b09fc07eab572faa3383eedf817
Dragon <dragonofmercy@gmail.com>
2023-05-02 08:37:18 +0200

    Add SettingsGroup control

---
+++ ImageViewer/Controls/SettingsGroup/SettingsGroup.Properties.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace ImageViewer.Controls;
+
+public partial class SettingsGroup : ItemsControl
+{
+    public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(nameof(Title), typeof(string), typeof(SettingsGroup), new PropertyMetadata(defaultValue: null, (d, _) => ((SettingsGroup)d).OnTitlePropertyChanged()));
+    
+    public string Title
+    {
+        get => (string)GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+    
+    protected virtual void OnTitlePropertyChanged()
+    {
+        OnTitleChanged();
+    }
+}
---
+++ ImageViewer/Controls/SettingsGroup/SettingsGroup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace ImageViewer.Controls;
+
+public partial class SettingsGroup : ItemsControl
+{
+    public SettingsGroup()
+    {
+        DefaultStyleKey = typeof(SettingsGroup);
+    }
+    
+    protected override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+        OnTitleChanged();
+    }
+    
+    private void OnTitleChanged()
+    {
+        if(GetTemplateChild("TitlePresenter") is FrameworkElement titlePresenter)
+        {
+            titlePresenter.Visibility = Title != null ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
